e5a14cfa43c15a30cc2fe27bbcbaf581f100dd99
SavinKG <savinkg@gmail.com>
2019-12-05 19:39:32 +0200

    first draft

--- include/impl.h
+++ include/impl.h
@@ -1,1 +1,24 @@
 #pragma once
+
+class Complex
+{
+private:
+    int _re;
+    int _im;
+public:
+    Complex(int re = 0, int im = 0);
+    virtual ~Complex();
+
+    int getRe() const
+    {
+        return _re;
+    }
+    int getIm() const
+    {
+        return _im;
+    }
+    constexpr Complex operator+(const Complex& op);
+    constexpr Complex operator-(const Complex& op);
+};
+
+constexpr bool operator==(const Complex &op1,const Complex &op2);
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,59 @@
 #include "impl.h"
+
+Complex::Complex(int re,int im): _re(re),_im(im)
+{
+    _re = re;
+    _im = im;
+}
+Complex::~Complex()
+{
+}
+
+constexpr Complex Complex::operator+(const Complex& op)
+{
+    Complex result;
+
+    result._re = _re + op.getRe();
+    result._im = _im + op.getIm();
+
+    return result;
+}
+
+constexpr Complex Complex::operator-(const Complex& op)
+{
+    Complex result;
+
+    result._re = _re - op.getRe();
+    result._im = _im - op.getIm();
+
+    return result;
+}
+
+constexpr Complex Complex::operator+=(const Complex& op)
+{
+    Complex result;
+
+    result._re = _re + op.getRe();
+    result._im = _im + op.getIm();
+
+    return result;
+}
+
+constexpr Complex Complex::operator-=(const Complex& op)
+{
+    Complex result;
+
+    result._re = _re - op.getRe();
+    result._im = _im - op.getIm();
+
+    return result;
+}
+constexpr bool Complex::operator==(const Complex& op1,const Complex& op2)
+{
+    return (op1.getRe() == op2.getRe()) && (op1.getIm() == op2.getIm());
+}
+
+constexpr Complex operator "" _i(int re, int im)
+{
+    return {re,im};
+}
